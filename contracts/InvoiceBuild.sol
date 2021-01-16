6de6076ecfa0c86ff33757f3773dab7c8a1629f1
Gareth Fuller <gareth.a.f@gmail.com>
2021-01-16 22:46:14 +0000

    Add withdrawal function

--- contracts/InvoiceBuild.sol
+++ contracts/InvoiceBuild.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721Burnable.sol";
+import "@openzeppelin/contracts/payment/escrow/Escrow.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
@@ -15,25 +16,31 @@ contract InvoiceBuild is ERC721, ERC721Burnable, Ownable {
 
     event InvoiceCreated(uint256 indexed id, uint256 amount, address indexed owner);
     event InvoicePayment(uint256 id, uint256 amount, address indexed payer);
+    event InvoiceWithdrawal(uint256 id, uint256 amount, address indexed recipient);
 
     constructor() public ERC721("Invoice", "INV") {}
 
     struct Invoice {
-      uint256 amount;
-      uint256 balance;
+      uint256 amount; // The total amount to be paid
+      uint256 outstanding; // The outstanding balance to be paid off
+      address payable recipient; // Who can withdraw any funds deposited
+      Escrow escrow;
     }
 
     mapping (uint256 => Invoice) internal _invoices;
+    mapping (uint256 => address) internal _invoiceOwner;
     mapping (address => uint256[]) internal _invoicesForOwner;
     mapping (address => uint256) internal _invoiceCountForOwner;
 
-    function create(uint256 amount, string memory metaUrl) public returns (uint256) {
+    function create(uint256 amount, address payable recipient, string memory metaUrl) public returns (uint256) {
         require(amount > 0, "Amount too low");
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
+        Escrow escrow = new Escrow();
 
-        _invoices[newItemId] = Invoice({ amount: amount, balance: amount });
+        _invoices[newItemId] = Invoice({ amount: amount, outstanding: amount, recipient: recipient, escrow: escrow });
+        _invoiceOwner[newItemId] = msg.sender;
         _invoicesForOwner[msg.sender].push(newItemId);
         _invoiceCountForOwner[msg.sender] = _invoiceCountForOwner[msg.sender].add(1);
 
@@ -44,12 +51,24 @@ contract InvoiceBuild is ERC721, ERC721Burnable, Ownable {
         return newItemId;
     }
 
-    function payInvoice(uint256 id) external payable {
-      require(msg.value <= invoiceBalance(id), "Amount greater than remaining balance");
+    function makePayment(uint256 id) external payable {
+      uint256 amount = msg.value;
+      require(amount <= invoiceOutstanding(id), "Amount greater than remaining balance");
+      
+      _invoices[id].outstanding = _invoices[id].outstanding.sub(amount);
+      _invoices[id].escrow.deposit{ value: msg.value }(_invoices[id].recipient);
 
-      _invoices[id].balance = _invoices[id].balance.sub(msg.value);
+      emit InvoicePayment(id, amount, msg.sender);
+    }
 
-      emit InvoicePayment(id, msg.value, msg.sender);
+    function withdrawBalance(uint256 id) public {
+      uint256 balance = invoiceBalance(id);
+      require(balance > 0, "Nothing to withdraw");
+      require(msg.sender == _invoiceOwner[id], "Caller is not the owner");
+
+      _invoices[id].escrow.withdraw(_invoices[id].recipient);
+
+      emit InvoiceWithdrawal(id, balance, msg.sender);
     }
 
     function invoicesForOwner(address account) public view returns (uint256[] memory) {
@@ -57,7 +76,15 @@ contract InvoiceBuild is ERC721, ERC721Burnable, Ownable {
     }
 
     function invoiceBalance(uint256 id) public view returns (uint256) {
-      return _invoices[id].balance;
+      return _invoices[id].escrow.depositsOf(_invoices[id].recipient);
+    }
+
+    function invoiceOutstanding(uint256 id) public view returns (uint256) {
+      return _invoices[id].outstanding;
+    }
+
+    function isPaid(uint256 id) public view returns (bool) {
+      return invoiceOutstanding(id) == 0;
     }
 
     function invoiceAmount(uint256 id) public view returns (uint256) {
--- test/invoice-build-test.js
+++ test/invoice-build-test.js
@@ -1,10 +1,10 @@
 const { expect } = require('chai')
 
 describe('InvoiceBuild contract', function() {
-  let InvoiceBuild, invoiceBuild, owner, signer1, signer2
+  let InvoiceBuild, invoiceBuild, owner, signer1, signer2, recipient1, recipient2
 
   beforeEach(async function () {
-    [owner, signer1, signer2] = await ethers.getSigners()
+    [owner, signer1, signer2, recipient1, recipient2] = await ethers.getSigners()
     InvoiceBuild = await ethers.getContractFactory('InvoiceBuild')
     invoiceBuild = await InvoiceBuild.deploy()
   })
@@ -22,7 +22,7 @@ describe('InvoiceBuild contract', function() {
       amount = ethers.utils.parseUnits('1000', 'ether')
       metaUrl = 'https://invoice.build'
 
-      await invoiceBuild.connect(signer1).create(amount, metaUrl)
+      await invoiceBuild.connect(signer1).create(amount, recipient1.address, metaUrl)
     })
 
     it('Should be owned by signer', async function () {
@@ -35,37 +35,43 @@ describe('InvoiceBuild contract', function() {
 
     it('should have unique tokenURIs', async function () {
       const metaUrl2 = 'https://invoice.build/2'
-      await invoiceBuild.connect(signer2).create(amount, metaUrl2)
+      await invoiceBuild.connect(signer2).create(amount, recipient2.address, metaUrl2)
       expect(await invoiceBuild.tokenURI(1)).to.equal(metaUrl)
       expect(await invoiceBuild.tokenURI(2)).to.equal(metaUrl2)
     })
 
     it('Should add id to owners invoice map', async function () {
-      await invoiceBuild.connect(signer2).create('5000', metaUrl)
-      await invoiceBuild.connect(signer1).create('3000', metaUrl)
+      await invoiceBuild.connect(signer2).create('5000', recipient2.address, metaUrl)
+      await invoiceBuild.connect(signer1).create('3000', recipient1.address, metaUrl)
       const ids = (await invoiceBuild.invoicesForOwner(signer1.address)).map(id => id.toNumber())
       expect(ids).to.eql([1,3])
     })
 
     it('Increments total supply', async function () {
-      await invoiceBuild.connect(signer2).create('5000', metaUrl)
+      await invoiceBuild.connect(signer2).create('5000', recipient2.address, metaUrl)
       const totalSupply = (await invoiceBuild.connect(owner).totalSupply()).toNumber()
       expect(totalSupply).to.equal(2)
     })
 
     it('Returns number of invoices owned', async function () {
-      await invoiceBuild.connect(signer1).create('5000', metaUrl)
-      await invoiceBuild.connect(signer2).create('3000', metaUrl)
+      await invoiceBuild.connect(signer1).create('5000', recipient1.address, metaUrl)
+      await invoiceBuild.connect(signer2).create('3000', recipient2.address, metaUrl)
       const count1 = (await invoiceBuild.connect(owner).balanceOf(signer1.address)).toNumber()
       const count2 = (await invoiceBuild.connect(owner).balanceOf(signer2.address)).toNumber()
       expect(count1).to.equal(2)
       expect(count2).to.equal(1)
     })
 
-    it('Returns balance for token', async function () {
+    it('Returns oustanding amount for token', async function () {
+      let outstanding = await invoiceBuild.invoiceOutstanding(1)
+      outstanding = parseFloat(ethers.utils.formatUnits(outstanding, 'ether'))
+      expect(outstanding).to.equal(1000.0)
+    })
+
+    it('Returns withdrawable balance for token', async function () {
       let balance = await invoiceBuild.invoiceBalance(1)
       balance = parseFloat(ethers.utils.formatUnits(balance, 'ether'))
-      expect(balance).to.equal(1000.0)
+      expect(balance).to.equal(0.0)
     })
 
     it('Returns amount for token', async function () {
@@ -76,7 +82,7 @@ describe('InvoiceBuild contract', function() {
 
     it('Prevents negative or zero amount invoice', async function () {
       try {
-        await invoiceBuild.connect(signer1).create('0', metaUrl)
+        await invoiceBuild.connect(signer1).create('0', recipient1.address, metaUrl)
       } catch (error) {
         expect(error.message).to.include('Amount too low')
       }
@@ -90,36 +96,93 @@ describe('InvoiceBuild contract', function() {
       amount = ethers.utils.parseUnits('1000', 'ether')
       metaUrl = 'https://invoice.build'
 
-      await invoiceBuild.connect(signer1).create(amount, metaUrl)
+      await invoiceBuild.connect(signer1).create(amount, recipient1.address, metaUrl)
     })
 
-    it('Reduces invoice balance', async function () {
+    it('Reduces invoice outstanding', async function () {
       const value = ethers.utils.parseUnits('150.5', 'ether').toHexString()
-      await invoiceBuild.connect(signer2).payInvoice(1, { value })
+      await invoiceBuild.connect(signer2).makePayment(1, { value })
 
-      let balance = await invoiceBuild.invoiceBalance(1)
-      balance = parseFloat(ethers.utils.formatUnits(balance, 'ether'))
+      let outstanding = await invoiceBuild.invoiceOutstanding(1)
+      outstanding = parseFloat(ethers.utils.formatUnits(outstanding, 'ether'))
 
-      expect(balance).to.equal(849.5)
+      expect(outstanding).to.equal(849.5)
     })
 
-    it('Increases the contract balance', async function () {
+    it('Doesnt increase the contract balance', async function () {
       const value = ethers.utils.parseUnits('150.5', 'ether').toHexString()
-      await invoiceBuild.connect(signer2).payInvoice(1, { value })
+      await invoiceBuild.connect(signer2).makePayment(1, { value })
 
       let contractBalance = await ethers.provider.getBalance(invoiceBuild.address)
       contractBalance = parseFloat(ethers.utils.formatUnits(contractBalance, 'ether'))
 
-      expect(contractBalance).to.equal(150.5)
+      expect(contractBalance).to.equal(0)
     })
 
     it('Prevents overpayment', async function () {
       try {
         const value = ethers.utils.parseUnits('1001', 'ether').toHexString()
-        await invoiceBuild.connect(signer2).payInvoice(1, { value })
+        await invoiceBuild.connect(signer2).makePayment(1, { value })
       } catch (error) {
         expect(error.message).to.include('Amount greater than remaining balance')
       }
+    })
+  })
+
+  describe('Withdrawals', function () {
+    let amount, metaUrl
+
+    beforeEach(async function () {
+      amount = ethers.utils.parseUnits('1000', 'ether')
+      metaUrl = 'https://invoice.build'
+    })
+
+    it('Fails if not owner', async function () {
+      await invoiceBuild.connect(signer1).create(amount, recipient1.address, metaUrl)
+      const value = ethers.utils.parseUnits('1000', 'ether').toHexString()
+      await invoiceBuild.connect(signer2).makePayment(1, { value })
+
+      try {
+        await invoiceBuild.connect(signer2).withdrawBalance(1)
+      } catch (error) {
+        expect(error.message).to.include('not the owner')
+      }
+    })
+
+    it('Fails if nothing to withdraw', async function () {
+      await invoiceBuild.connect(signer1).create(amount, recipient1.address, metaUrl)
+
+      try {
+        await invoiceBuild.connect(signer1).withdrawBalance(1)
+      } catch (error) {
+        expect(error.message).to.include('Nothing to withdraw')
+      }
+    })
+
+    it('Fails if already fully withdrawn', async function () {
+      await invoiceBuild.connect(signer1).create(amount, recipient1.address, metaUrl)
+      const value = ethers.utils.parseUnits('1000', 'ether').toHexString()
+      await invoiceBuild.connect(signer2).makePayment(1, { value })
+      await invoiceBuild.connect(signer1).withdrawBalance(1)
+
+      try {
+        await invoiceBuild.connect(signer1).withdrawBalance(1)
+      } catch (error) {
+        expect(error.message).to.include('Nothing to withdraw')
+      }
+    })
+
+    it('Increases balance of recipient', async function () {
+      let startinBalance = await ethers.provider.getBalance(recipient1.address)
+      startinBalance = parseFloat(ethers.utils.formatUnits(startinBalance, 'ether'))
+      await invoiceBuild.connect(signer1).create(amount, recipient1.address, metaUrl)
+      const value = ethers.utils.parseUnits('500', 'ether').toHexString()
+      await invoiceBuild.connect(signer2).makePayment(1, { value })
+      await invoiceBuild.connect(signer1).withdrawBalance(1)
+      let newBalance = await ethers.provider.getBalance(recipient1.address)
+      newBalance = parseFloat(ethers.utils.formatUnits(newBalance, 'ether'))
+      
+      expect(newBalance - startinBalance).to.equal(500.0)
     })
   })
 })
