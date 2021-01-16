f76e9b8af2a13b5529aa4bf4d6cf26235c3cee80
Gareth Fuller <gareth.a.f@gmail.com>
2021-01-16 16:13:52 +0000

    Basic invoice NFT contract

--- contracts/InvoiceBuild.sol
+++ contracts/InvoiceBuild.sol
@@ -2,21 +2,65 @@
 pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
-contract InvoiceBuild is ERC721 {
+contract InvoiceBuild is ERC721, ERC721Burnable, Ownable {
     using Counters for Counters.Counter;
+    using SafeMath for uint256;
+
     Counters.Counter private _tokenIds;
+
+    event InvoiceCreated(uint256 indexed id, uint256 amount, address indexed owner);
+    event InvoicePayment(uint256 id, uint256 amount, address indexed payer);
 
     constructor() public ERC721("Invoice", "INV") {}
 
-    function create(string memory tokenURI) public returns (uint256) {
+    struct Invoice {
+      uint256 amount;
+      uint256 balance;
+    }
+
+    mapping (uint256 => Invoice) internal _invoices;
+    mapping (address => uint256[]) internal _invoicesForOwner;
+    mapping (address => uint256) internal _invoiceCountForOwner;
+
+    function create(uint256 amount, string memory metaUrl) public returns (uint256) {
+        require(amount > 0, "Amount too low");
         _tokenIds.increment();
 
         uint256 newItemId = _tokenIds.current();
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
 
+        _invoices[newItemId] = Invoice({ amount: amount, balance: amount });
+        _invoicesForOwner[msg.sender].push(newItemId);
+        _invoiceCountForOwner[msg.sender] = _invoiceCountForOwner[msg.sender].add(1);
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, metaUrl);
+
+        emit InvoiceCreated(newItemId, amount, msg.sender);
         return newItemId;
+    }
+
+    function payInvoice(uint256 id) external payable {
+      require(msg.value <= invoiceBalance(id), "Amount greater than remaining balance");
+
+      _invoices[id].balance = _invoices[id].balance.sub(msg.value);
+
+      emit InvoicePayment(id, msg.value, msg.sender);
+    }
+
+    function invoicesForOwner(address account) public view returns (uint256[] memory) {
+      return _invoicesForOwner[account];
+    }
+
+    function invoiceBalance(uint256 id) public view returns (uint256) {
+      return _invoices[id].balance;
+    }
+
+    function invoiceAmount(uint256 id) public view returns (uint256) {
+      return _invoices[id].amount;
     }
 }
--- test/invoice-build-test.js
+++ test/invoice-build-test.js
@@ -1,20 +1,106 @@
-const { expect } = require("chai")
+const { expect } = require('chai')
 
-describe("InvoiceBuild", function() {
-  it("Testing...", async function() {
-    const [signer] = await ethers.getSigners();
-    const InvoiceBuild = await ethers.getContractFactory("InvoiceBuild")
-    const invoiceBuild = await InvoiceBuild.deploy()
-    
-    await invoiceBuild.deployed()
+describe('InvoiceBuild contract', function() {
+  let InvoiceBuild, invoiceBuild, owner, signer1, signer2
 
-    await invoiceBuild.create('https://invoice.build')
-    const owner = await invoiceBuild.ownerOf(1)
-    expect(owner).to.equal(signer.address)
+  beforeEach(async function () {
+    [owner, signer1, signer2] = await ethers.getSigners()
+    InvoiceBuild = await ethers.getContractFactory('InvoiceBuild')
+    invoiceBuild = await InvoiceBuild.deploy()
+  })
 
-    // expect(await invoiceBuild.create('https://invoice.build')).to.equal("Hello, world!")
+  describe('Deployment', function () {
+    it('Should set the right owner', async function () {
+      expect(await invoiceBuild.owner()).to.equal(owner.address)
+    })
+  })
 
-    // await greeter.setGreeting("Hola, mundo!")
-    // expect(await greeter.greet()).to.equal("Hola, mundo!")
+  describe('Minting', function () {
+    let amount, metaUrl
+
+    beforeEach(async function () {
+      amount = ethers.utils.parseUnits('1000', 'ether')
+      metaUrl = 'https://invoice.build'
+
+      await invoiceBuild.connect(signer1).create(amount, metaUrl)
+    })
+
+    it('Should be owned by signer', async function () {
+      expect(await invoiceBuild.ownerOf(1)).to.equal(signer1.address)
+    })
+
+    it('Should have correct tokenURI', async function () {
+      expect(await invoiceBuild.tokenURI(1)).to.equal(metaUrl)
+    })
+
+    it('should have unique tokenURIs', async function () {
+      const metaUrl2 = 'https://invoice.build/2'
+      await invoiceBuild.connect(signer2).create(amount, metaUrl2)
+      expect(await invoiceBuild.tokenURI(1)).to.equal(metaUrl)
+      expect(await invoiceBuild.tokenURI(2)).to.equal(metaUrl2)
+    })
+
+    it('Should add id to owners invoice map', async function () {
+      await invoiceBuild.connect(signer2).create('5000', metaUrl)
+      await invoiceBuild.connect(signer1).create('3000', metaUrl)
+      const ids = (await invoiceBuild.invoicesForOwner(signer1.address)).map(id => id.toNumber())
+      expect(ids).to.eql([1,3])
+    })
+
+    it('Increments total supply', async function () {
+      await invoiceBuild.connect(signer2).create('5000', metaUrl)
+      const totalSupply = (await invoiceBuild.connect(owner).totalSupply()).toNumber()
+      expect(totalSupply).to.equal(2)
+    })
+
+    it('Returns number of invoices owned', async function () {
+      await invoiceBuild.connect(signer1).create('5000', metaUrl)
+      await invoiceBuild.connect(signer2).create('3000', metaUrl)
+      const count1 = (await invoiceBuild.connect(owner).balanceOf(signer1.address)).toNumber()
+      const count2 = (await invoiceBuild.connect(owner).balanceOf(signer2.address)).toNumber()
+      expect(count1).to.equal(2)
+      expect(count2).to.equal(1)
+    })
+
+    it('Returns balance for token', async function () {
+      let balance = await invoiceBuild.invoiceBalance(1)
+      balance = parseFloat(ethers.utils.formatUnits(balance, 'ether'))
+      expect(balance).to.equal(1000.0)
+    })
+
+    it('Returns amount for token', async function () {
+      let amount = await invoiceBuild.invoiceAmount(1)
+      amount = parseFloat(ethers.utils.formatUnits(amount, 'ether'))
+      expect(amount).to.equal(1000.0)
+    })
+
+    it('Prevents negative or zero amount invoice', async function () {
+      try {
+        await invoiceBuild.connect(signer1).create('0', metaUrl)
+      } catch (error) {
+        expect(error.message).to.include('Amount too low')
+      }
+    })
+  })
+
+  describe('Payments', function () {
+    let amount, metaUrl
+
+    beforeEach(async function () {
+      amount = ethers.utils.parseUnits('1000', 'ether')
+      metaUrl = 'https://invoice.build'
+
+      await invoiceBuild.connect(signer1).create(amount, metaUrl)
+    })
+
+    it('Reduces invoice balance', async function () {
+      const value = ethers.utils.parseUnits('150.5', 'ether').toHexString()
+      await invoiceBuild.connect(signer2).payInvoice(1, { value })
+
+      let balance = await invoiceBuild.invoiceBalance(1)
+      balance = parseFloat(ethers.utils.formatUnits(balance, 'ether'))
+
+      expect(balance).to.equal(849.5)
+    })
   })
 })
